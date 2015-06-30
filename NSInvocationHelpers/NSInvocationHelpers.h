d8872c563f66f2468dfacc1a94ee993bf82ddfbd
stephane-fueled <stephane@fueled.com>
2015-06-30 14:58:00 -0400

    feat(project): add framework Xcode project

---
+++ NSInvocationHelpers/NSInvocationHelpers.h
@@ -0,0 +1,19 @@
+//
+//  NSInvocationHelpers.h
+//  NSInvocationHelpers
+//
+//  Created by Stephane Copin on 6/30/15.
+//  Copyright (c) 2015 Stephane Copin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSInvocationHelpers.
+FOUNDATION_EXPORT double NSInvocationHelpersVersionNumber;
+
+//! Project version string for NSInvocationHelpers.
+FOUNDATION_EXPORT const unsigned char NSInvocationHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSInvocationHelpers/PublicHeader.h>
+
+
---
+++ NSInvocationHelpersTests/NSInvocationHelpersTests.m
@@ -0,0 +1,26 @@
+//
+//  NSInvocationHelpersTests.m
+//  NSInvocationHelpersTests
+//
+//  Created by Stephane Copin on 6/30/15.
+//  Copyright (c) 2015 Stephane Copin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NSInvocationHelpersTests : XCTestCase
+
+@end
+
+@implementation NSInvocationHelpersTests
+
+- (void)setUp {
+  [super setUp];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+@end
