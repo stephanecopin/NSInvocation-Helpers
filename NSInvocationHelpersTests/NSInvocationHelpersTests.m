cd374ca09bd4f4d5abd3b23ba146e209216ca34e
stephane-fueled <stephane@fueled.com>
2015-06-30 15:09:33 -0400

    test(project): add a unit test

--- NSInvocationHelpersTests/NSInvocationHelpersTests.m
+++ NSInvocationHelpersTests/NSInvocationHelpersTests.m
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "NSInvocation+Helpers.h"
+
 @interface NSInvocationHelpersTests : XCTestCase
 
 @end
@@ -21,6 +23,20 @@
 
 - (void)tearDown {
   [super tearDown];
+}
+
+- (void)testInvocationCategory {
+  NSInvocation * invocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:@selector(methodSignatureExample:)]];
+  [invocation setSelector:@selector(methodSignatureExample:)];
+  [invocation setTarget:self];
+  NSInteger parameter = 42;
+  [invocation setArgument:&parameter atIndex:2];
+  [invocation invoke];
+  XCTAssertTrue([[invocation objectReturnValue] isEqualToNumber:@(parameter)]);
+}
+
+- (NSNumber *)methodSignatureExample:(NSInteger)parameter {
+  return @(parameter);
 }
 
 @end
